6e93a03467cdde3612f2e5ca833364a42b10c2ca
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-08-19 17:07:22 +0530

    DerpFest => RisingUI
    * Build Core GMs
    * Don't Build Aperture By Default 
    * Don't Use Google Dialer

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -2,21 +2,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common DerpFestOS stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common RisingOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Build Flags
+RISING_MAINTAINER := "Rohit-Tiwari"
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_USE_GOOGLE_TELEPHONY := false
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_NOW_PLAYING := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_CORE_GMS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := derp_fleur
+PRODUCT_NAME := lineage_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
