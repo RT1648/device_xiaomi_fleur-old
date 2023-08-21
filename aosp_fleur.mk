7bd90f8ea3ba211e4e36c47bc9e24802406e5bbe
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-08-21 16:15:47 +0530

    Superior => Aosp

--- aosp_fleur.mk
+++ aosp_fleur.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common SuperiorOS stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -12,14 +12,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_ENABLE_BLUR := true
-USE_MOTO_CALCULATOR := true
-IS_PHONE := true
-BUILD_WITH_GAPPS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := superior_fleur
+PRODUCT_NAME := aosp_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
