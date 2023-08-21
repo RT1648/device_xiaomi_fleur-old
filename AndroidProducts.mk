3c715b2beb9a8a769760bc5721f4bef57df6f40a
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-08-21 16:16:50 +0530

    Superior => Aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_fleur.mk
+    $(LOCAL_DIR)/aosp_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_fleur-user \
-    superior_fleur-userdebug \
-    superior_fleur-eng
+    aosp_fleur-user \
+    aosp_fleur-userdebug \
+    aosp_fleur-eng
