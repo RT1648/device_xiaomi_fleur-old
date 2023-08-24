61107d156a66c9be65c1909e807d237a3a9088d7
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-08-24 09:48:25 +0530

    Superior => EvolutionX

--- evolution_fleur.mk
+++ evolution_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Evolutionx stuff
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -13,15 +13,15 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
-USE_MOTO_CALCULATOR := true
-IS_PHONE := true
-BUILD_WITH_GAPPS := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := superior_fleur
+PRODUCT_NAME := evolution_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
