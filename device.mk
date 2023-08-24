0d08c7e09099bcc9e3dbac0175d0cee304fe7957
ShazuxD <shazuxd@proton.me>
2023-08-24 18:09:29 +0530

    Fleur: Fix Some Typos
    Signed-off-by: RT1648 <rohitttiwari52@gamil.com>

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_PACKAGES += \
     WifiOverlayFleur \
     SystemUIOverlayFleur \
     SettingsOverlayFleur \
-    SimpleDeviceConfigOverlayfleur
+    SimpleDeviceConfigOverlayFleur
 
 # Runtine Device Name Overlays
 PRODUCT_PACKAGES += \
