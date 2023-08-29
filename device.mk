7a548bffc23292718ec613352221c7cefd6dfc99
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-29 23:55:30 +0530

    Fleur: overlay: Show `Turbo Charging` instead of `Charging Rapidly`
    * Like MIUI
    
    * xawlw: Adapt to rro
    
    Change-Id: Ie3f916be54e65d76ae3be3df62bd6e54db280159
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -182,6 +182,8 @@ PRODUCT_PACKAGES += \
     FleurpSettingsProviderOverlay \
     MielSettingsProviderOverlay \
     MielpSettingsProviderOverlay
+    SettingsLibOverlayFleur \
+    SimpleDeviceConfigOverlayFleur
 
 # Permissions
 PRODUCT_COPY_FILES += \
