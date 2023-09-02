0be3cc3d01c6f79529bba6e5b142fe76a89886d3
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-09-02 13:36:26 +0530

    Colt => Bliss

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/colt_fleur.mk
+    $(LOCAL_DIR)/bliss_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    colt_fleur-user \
-    colt_fleur-userdebug \
-    colt_fleur-eng
+    bliss_fleur-user \
+    bliss_fleur-userdebug \
+    bliss_fleur-eng
