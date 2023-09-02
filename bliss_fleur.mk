c477f88aa9cbf4d1021089234b5b85d936b4fc68
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-09-02 13:35:41 +0530

    Colt => Bliss

--- bliss_fleur.mk
+++ bliss_fleur.mk
@@ -3,24 +3,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ColtOS stuff
-$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Build Flags
-COLT_BUILD_MAINTAINER := Rohit-Tiwari
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_NOW_PLAYING := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-WITH_GAPPS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := colt_fleur
+PRODUCT_NAME := bliss_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
