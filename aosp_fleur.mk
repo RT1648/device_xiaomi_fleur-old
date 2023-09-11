7893ba3f2a140c050ee0e410931a2b1d1595e4d5
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-11 21:29:12 +0530

    Fleur: Add Some Missing Flags
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- aosp_fleur.mk
+++ aosp_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common PixelExperience stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -12,12 +12,16 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
 PRODUCT_NAME := aosp_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
