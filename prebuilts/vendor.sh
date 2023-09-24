fc4d7d0421a4ebe86274195b4e6ffd079e7d351e
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-09-24 10:37:09 +0530

    Fleur: Drop Prebuilts
    * Not required anymore, moved to d/f repo
    
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- prebuilts/vendor.sh
+++
@@ -1,4 +0,0 @@
-echo "Downloading Prebuilt Vendor, Please Wait A Few Minutes"
-cd device/xiaomi/fleur/prebuilts || echo "Make Sure You Are Running This Script Only From Root Directory Of Rom Source"
-wget https://github.com/RT1648/Extras/releases/download/13.0.9.0/vendor.img
-echo "Download Completed, Now You Can Build"
